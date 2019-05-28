34f24591bdd3ade016bea46ae30e2c705647f113
Nick Sun <Nick_Sun@asus.com>
2019-05-28 14:56:44 +0800

    Project init

---
+++ AsuNetMvcVueTemplate/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const html = document.querySelector('html')
+  let fontSize = window.innerWidth / 7.2
+  html.style.fontSize = fontSize + 'px'
+})
+
+</script>
+
+<style lang="scss">
+@import "@/assets/styles/global.scss";
+</style>
---
+++ AsuNetMvcVueTemplate/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    abc
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  components: {}
+}
+</script>
